7c98b0aff941c25c33260993da3d5a7c4927d38f
Niko Gham <infoscouts@protonmail.com>
2020-05-04 12:01:25 +0100

    card display

--- index.js
+++ index.js
@@ -3,18 +3,41 @@ function init() {
     var btn = document.createElement("BUTTON");
     btn.innerHTML = "Monsters";
     document.body.appendChild(btn);
-
+    // Don't have to go through this process to add ID but oh well // 
     var list = document.getElementsByTagName("button")[0];
 
     list.id ="monsters";
-
+// to here // 
     var mons = document.getElementById("monsters");
 
     mons.addEventListener("click", function(){
         alert(`${deck.monsters}`);
     });
+    
+// Magic Button // 
+
+    var btn1 = document.createElement("BUTTON");
+    btn1.innerHTML = "Magics";
+    document.body.appendChild(btn1);
+    btn1.id = "magics";
+    var mag = document.getElementById("magics");
+    mag.addEventListener("click", function() {
+        alert(`${deck.magics}`);
+    });
+
+// Trap Button // 
+    var btn2 = document.createElement("BUTTON");
+    btn2.innerHTML = "Traps";
+    document.body.appendChild(btn2);
+    btn2.id = "traps";
+    var tra = document.getElementById("traps");
+    tra.addEventListener("click", function(){
+        alert(`This deck has ${deck.traps} traps`);
+    })
 
     
+
+
 
 
     const deck = {
