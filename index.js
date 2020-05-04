c1955ea87344f7488a3d881ecf2024980edcf041
Niko Gham <infoscouts@protonmail.com>
2020-05-04 11:36:01 +0100

    whoo a click worked

---
+++ index.js
@@ -0,0 +1,34 @@
+function init() {
+
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Monsters";
+    document.body.appendChild(btn);
+
+    var list = document.getElementsByTagName("button")[0];
+
+    list.id ="monsters";
+
+    var mons = document.getElementById("monsters");
+
+    mons.addEventListener("click", function(){
+        alert("hello");
+    })
+
+    
+
+
+    const deck = {
+        cards: 50, 
+        monsters: 23, 
+        magics: 10, 
+        traps: 7, 
+        fusions: 10
+    };
+
+  
+
+  
+
+    
+}
+document.addEventListener("DOMContentLoaded", init, false);
